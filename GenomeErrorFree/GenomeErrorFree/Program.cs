6b09d256cb8cac64ca2817a640f863e46ee53a99
Jim Stewart <jimboweb@gmail.com>
2017-12-15 17:18:13 -0500

    input

--- GenomeErrorFree/GenomeErrorFree/Program.cs
+++ GenomeErrorFree/GenomeErrorFree/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace GenomeErrorFree
 {
-    class Program
+    class GenomeErrorFree
     {
+        int inputSize = 1618; 
         static void Main(string[] args)
         {
+            var gef = new GenomeErrorFree();
+            gef.run();
+        }
+        private void run()
+        {
+            var input = new string[inputSize];
+            for (int i = 0; i < inputSize; i++)
+            {
+                input[i] = Console.ReadLine();
+            }
+
+
         }
     }
 }
